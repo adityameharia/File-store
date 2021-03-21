29a308f476da2867746e1495059668d4c3fc8f39
Aditya Meharia <adityameharia14@gmail.com>
2021-03-22 00:50:50 +0530

    can upload files now

--- frontend/src/components/File.js
+++ frontend/src/components/File.js
@@ -4,8 +4,10 @@ import { useHistory } from 'react-router-dom';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 import { refreshTokenSetup } from '../utils/refreshToken';
+import setToken from '../utils/setToken'
 
 import NavbarCustom from '../layout/Navbar'
+import axios from 'axios';
 
 const clientId =
 
@@ -16,16 +18,70 @@ const clientId =
 function LoginHooks() {
 
     let history = useHistory();
-
+    
+    let [filename,setFileName]=useState()
     let [loading, setLoading] = useState(true)
+    let [selectedFile, setSelectedFile] = useState();
+    let [isFilePicked, setIsFilePicked] = useState(false);
+    let [userData, setUserData] = useState()
 
-    const onSuccess = (res) => {
-        setLoading(false)
-        refreshTokenSetup(res);
-        console.log(res);
+    const changeHandler = (event) => {      
+        if(event.target.files[0]!=undefined)  
+        setFileName(event.target.files[0].name)
+        setSelectedFile(event.target.files[0]);
+        setIsFilePicked(true);
+    };
+
+    const handleSubmission = async (e) => {
+        e.preventDefault()
+        
+        if (!isFilePicked) {
+            console.log("pick file pls")
+            return
+        }
+
+        const data = new FormData()
+        data.append('file', selectedFile)        
+
+        if(userData.files.includes(filename)){
+            alert("a file with the given name already exists")
+            return
+        }
+
+        try {
+            let res = await axios.post("/upload", data)
+            axios.get('/home').then(response=>{
+                setUserData(response.data)  
+            })
+            
+            alert("uploaded")
+            
+        }
+        catch(err){
+            console.log(err)
+        }
+    };
+
+    const onSuccess = async (res) => {
+
+        setToken(res.tokenId)
+        try {
+            let response = await axios.get('/home')
+            
+            setLoading(false)
+            refreshTokenSetup(res);
+            setUserData(response.data)
+
+        }
+        catch (err) {
+            alert(err.response.data.data)
+
+            history.push('/login')
+        }
     };
 
     const onAutoLoadFinished = (res) => {
+        console.log(res)
         if (res == false)
             history.push('/login')
     }
@@ -41,13 +97,16 @@ function LoginHooks() {
     return (
         <div>
             {loading ? (<div style={{ textAlign: "center", padding: "2rem" }}>
-                    <Loader
-                        type="Puff"
-                        color="#00BFFF"
-                        height={100}
-                        width={100}
-                    />
-                </div>):(<NavbarCustom />)}
+                <Loader
+                    type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                />
+            </div>) : (<div><NavbarCustom /><input type="file" name="file" onChange={changeHandler} />
+                <div>
+                    <button onClick={handleSubmission}>Submit</button>
+                </div></div>)}
         </div>
     );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,50 +1,73 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { GoogleLogin } from 'react-google-login';
 import axios from 'axios';
+import setToken from '../utils/setToken'
+
 const Login = () => {
   const clientId =
     '272242194309-sdgchprjq0s7auu186ofilo3o9ij6eir.apps.googleusercontent.com';
 
   let history = useHistory();
 
+
+  const Success = async (res) => {
     
-    const onSuccess =async (res) => {
-      await axios.post('http://localhost:8080/',{token:res.tokenId})
-      console.log(res)
+    
+    setToken(res.tokenId)
+    try {
+      await axios.post('/register', { name: res.profileObj.name, email: res.profileObj.email })
+      
       history.push('/')
-    };
+    }
+    catch (err) {
+      alert(err.response.data.data)      
+    }
+  };
 
-    const onFailure = (res) => {
-      console.log('Login failed: res:', res);
-      alert(
-        `Failed to login.`
-      );
-    };
-
-    return (
-      <div>
-        <GoogleLogin
-          clientId={clientId}
-          onSuccess={onSuccess}
-          buttonText="Login"
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          style={{ marginTop: '100px' }}
-          isSignedIn={true}
-        />
-        <GoogleLogin
-          clientId={clientId}
-          onSuccess={onSuccess}
-          buttonText="sign up"
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          style={{ marginTop: '100px' }}
-          isSignedIn={true}
-        />
-      </div>
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login.`
     );
-  
+  };
+
+  const onSuccess = async (res) => {
+    
+   
+    setToken(res.tokenId)
+    try {
+      await axios.post('/checkuser', { name: res.profileObj.name, email: res.profileObj.email })
+      
+      history.push('/')
+    } catch (error) {      
+      alert(error.response.data.data)
+    }
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        onSuccess={onSuccess}
+        buttonText="Login"
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+      <GoogleLogin
+        clientId={clientId}
+        onSuccess={Success}
+        buttonText="sign up"
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
+
 }
 export default Login;
 
